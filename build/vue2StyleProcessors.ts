e0d6364aab24e13ab39a40a5089f33dd58a4b315
Rémi Alvergnat <toilal.dev@gmail.com>
2021-03-09 17:47:55 +0100

    wip(vue2): fix typings for StylePreprocessor interface

--- build/vue2StyleProcessors.ts
+++ build/vue2StyleProcessors.ts
@@ -11,8 +11,7 @@ export interface StylePreprocessor {
   render(
     source: string,
     map: any | null,
-    options: any,
-    customRequire?: (id: string) => any
+    options: any
   ): StylePreprocessorResults
 }
 
